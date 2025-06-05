5e8050b33d1d4d42bc25bac882f0f7370d0df5eb
ndktr <ndktr.git@gmail.com>
2025-06-05 11:47:49 +0900

    fix: Remove one interface file

--- src/main/java/io/github/noooda/employee/domain/interfaces/CodeEvaluable.java
+++
@@ -1,6 +0,0 @@
-package io.github.noooda.employee.domain.interfaces;
-
-public interface CodeEvaluatable {
-    public void evaluateCode(String code);
-    public void provideFeedback(String code, String feedback);
-}
